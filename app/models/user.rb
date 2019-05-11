c6614b249fde2a090b0355f752357a8d5b9be360
taka.usu <liverpenguin1408@gmail.com>
2019-05-11 13:41:51 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  #バリデーション
+  #Userインスタンスを保存する前に行う
+  #文字をすべて小文字に変換
+  before_save {self.email.downcase!}
+  validates :name, presence:true,length:{maximum: 50}
+  validates :email,presence:true,length:{maximum: 255},
+                   #正規表現
+                   format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+                   #重複を許さない
+                   uniqueness: { case_sensitive: false }
+  
+  #パスワード付きのモデルを作成するため
+  #モデル作成時に、password_digestが必要
+  #Gemfileには、bcrypt Gemも必要
+  has_secure_password
+end
