61e4f1b0405d06243d6eab95e73ee62d75f923c6
taka.usu <liverpenguin1408@gmail.com>
2019-05-12 06:53:47 +0000

    user-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in , only:[:index,:show]
+  
   def index
     #.page(parame[;page]).per(25) → ページネーション
     @users = User.order(id: :desc).page(params[:page]).per(25)
