8b151e9e96d91936a30e42e1fba61b38424ba1cb
taka.usu <liverpenguin1408@gmail.com>
2019-05-25 05:49:55 +0000

    follow,unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,29 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  #relationshipsとの関係
+  has_many :relationships
+  #上記relationshipsを通じて、follow_idを参照し、followingsをみる
+  has_many :followings, through: :relationships, source: :follow
+  
+  has_many :reverse_of_relationships,class_name: 'Relationship',foreign_key: 'follow_id'
+  has_many :followers,through: :reverse_of_relationships,source: :user
+  
+  #other_userが自分でないか、既にフォローしていないか
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+    
+  #フォローしていれば、フォローを外す
+  def unfollow(other_user)
+    relationships = self.relationships.find_by(follow_id: other_user.id)
+    relationships.destroy if relationships
+  end
+  
+  #
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
